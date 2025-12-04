9d104a1273fa01332eda5fcfeab2a6832a42b624
tompernet2 <tom.pernet2@gmail.com>
2025-12-04 02:14:01 +0100

    navbar desktop

--- src/components/navbar/NavbarDesktop.jsx
+++ src/components/navbar/NavbarDesktop.jsx
@@ -1,37 +1,33 @@
 import { useState } from "react";
-import { CgCalendarDates, CgProfile, CgEricsson } from "react-icons/cg";
+import { CgCalendarDates, CgProfile, CgEricsson, CgChevronDoubleRight, CgChevronDoubleLeft } from "react-icons/cg";
 import NavButton from "../ui/NavButton";
 
 export default function NavbarDesktop() {
   const [mini, setMini] = useState(false);
 
   return (
-    <div
-      className={`hidden md:flex flex-col h-screen bg-blue-700 text-white p-4 transition-all duration-300 ${mini ? "w-20" : "w-64"
-        }`}
-    >
+    <div className={`hidden md:flex flex-col  bg-secondary text-white p-4 m-2 rounded-2xl transition-all duration-300 ${mini ? "w-20" : "w-max"}`}>
       {/* Bouton réduire/agrandir et logo */}
       <div className="flex flex-col items-center justify-center gap-2">
         {/* Bouton réduire/agrandir */}
-        <div className="flex justify-center mb-8">
-          <button
-            onClick={() => setMini(!mini)}
-            className="bg-blue-600 hover:bg-blue-500 p-2 rounded"
-          >
-            {mini ? "➤" : "◀"}
+        <div className="flex items-center justify-center mb-3 gap-2 ">
+
+          {/* Logo */}
+          {!mini && (
+            <div className="text-xl font-secondary">Planify</div>
+          )}
+
+          <button onClick={() => setMini(!mini)}>
+            {mini ? <CgChevronDoubleRight size={34} /> : <CgChevronDoubleLeft size={34} />}
           </button>
+
         </div>
 
-        {/* Logo */}
-        {!mini && (
-          <div className="flex items-center justify-center mb-6">
-            <div className="text-xl font-bold">Planify</div>
-          </div>
-        )}
+
       </div>
 
       {/* Liens du menu avec scroll vertical si nécessaire */}
-      <nav className="flex flex-col gap-4 items-center overflow-y-auto h-full pb-4">
+      <nav className="flex flex-col gap-4 overflow-y-auto h-full pb-4">
         <NavButton to="/" icon={CgProfile} label="Planning" mini={mini} />
         <NavButton to="/login" icon={CgCalendarDates} label="Login" mini={mini} />
         <NavButton to="/register" icon={CgEricsson} label="Register" mini={mini} />
